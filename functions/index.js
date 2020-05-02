06881af0a8b25807767cdf6ce57e101a33a7e311
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-05-02 20:26:36 +0530

    added notification

---
+++ functions/index.js
@@ -0,0 +1,126 @@
+/* eslint-disable handle-callback-err */
+/* eslint-disable promise/no-nesting */
+/* eslint-disable promise/always-return */
+const functions = require("firebase-functions");
+const app = require("express")();
+const admin = require("firebase-admin");
+const bodyParser = require("body-parser");
+const serviceAccount = require("./avinashvidyarthi-5f9076cc9fd2.json");
+const webPush = require("web-push");
+
+webPush.setVapidDetails(
+  "mailto:avinashvidyarthi@gmail.com",
+  "BKrd_OpQgyPWKHmyyHshZvRCOVMum_wDI7z4zQ-ZWvcHljHhnxMc14ir7G8B96xTXPrCu9xHKiTkSvoudXQTgRo",
+  "nU7KY-0jXCZ9693s52p27xqR69jhjJ3lcw49ypWG9C0"
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://avinashvidyarthi.firebaseio.com",
+});
+
+let db = admin.firestore();
+
+//header configuration
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Origin,X-Requested-With,Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/check", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.post("/store-subs", (req, res) => {
+  db.collection("subscription")
+    .doc()
+    .set(JSON.parse(req.body))
+    .then((data) => {
+      res.json({ status: "ok", msg: "subscribed" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/test-notification", (req, res) => {
+  db.collection("subscription")
+    .get()
+    .then((subs) => {
+      subs.forEach((s) => {
+        var options = {
+          endpoint: s.data().endpoint,
+          keys: {
+            auth: s.data().keys.auth,
+            p256dh: s.data().keys.p256dh,
+          },
+        };
+        webPush
+          .sendNotification(
+            options,
+            JSON.stringify({
+              title: "Test Notification!",
+              content: "This is a test notification",
+              url: "https://avinashvidyarthi.github.io"
+            })
+          )
+          .then((d) => {
+            res.json({ status: "ok", msg: d });
+          })
+          .catch((err) => {
+            // console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/notify",(req,res)=>{
+    db.collection("subscription")
+    .get()
+    .then((subs) => {
+      subs.forEach((s) => {
+        var options = {
+          endpoint: s.data().endpoint,
+          keys: {
+            auth: s.data().keys.auth,
+            p256dh: s.data().keys.p256dh,
+          },
+        };
+        webPush
+          .sendNotification(
+            options,
+            JSON.stringify({
+              title: req.body.title,
+              content: req.body.content,
+              url: req.body.link,
+              image:req.body.img_link==="" || req.body.img_link===null?null:req.body.img_link
+            })
+          )
+          .then((d) => {
+            res.json({ status: "ok", msg: d });
+          })
+          .catch((err) => {
+            // console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+exports.app = functions.https.onRequest(app);
--- js/resume.js
+++ js/resume.js
@@ -37,3 +37,93 @@ if('serviceWorker' in navigator){
 window.addEventListener('beforeinstallprompt',function(event){
   event.preventDefault();
 })
+
+
+// showing enable notification btn
+if('notification' in window && 'serviceWorker' in navigator){
+  if(window.Notification.permission!=='granted'){
+    $(".enable-notification").removeClass('d-none');
+  }
+}
+
+function configurePushSub(){
+  var sw_reg=null;
+  navigator.serviceWorker.ready
+  .then((swReg)=>{
+    sw_reg=swReg;
+    return swReg.pushManager.getSubscription();
+  })
+  .then((sub)=>{
+    if(sub===null){
+      // create subscription
+      // nU7KY-0jXCZ9693s52p27xqR69jhjJ3lcw49ypWG9C0
+      var vapidKey="BKrd_OpQgyPWKHmyyHshZvRCOVMum_wDI7z4zQ-ZWvcHljHhnxMc14ir7G8B96xTXPrCu9xHKiTkSvoudXQTgRo";
+      var convertedVapidKey=urlBase64ToUint8Array(vapidKey);
+      return sw_reg.pushManager.subscribe({
+        userVisibleOnly:true,
+        applicationServerKey:convertedVapidKey
+      });
+    }
+  })
+  .then((newSub)=>{
+    // store into server
+    return fetch("https://us-central1-avinashvidyarthi.cloudfunctions.net/app/store-subs",{
+      method:'POST',
+      body:JSON.stringify(newSub)
+    });
+  })
+  .then((res)=>{
+    if(res.ok){
+      welcomeNotification();
+    }
+  })
+  .catch((err)=>{
+    // console.log(err);
+  })
+}
+
+function welcomeNotification(){
+  if('serviceWorker' in navigator){
+    var options={
+      body:"You'll now get notifications from me.",
+      icon:"../img/icons/icons-96x96.png",
+      badge:"../img/icons/icons-96x96.png",
+      vibrate:[100,50,200],
+      data:{
+        url:"https://avinashvidyarthi.github.io"
+      }
+    }
+    navigator.serviceWorker.ready.then((swReg)=>{
+      swReg.showNotification("Successfully Subscribed!",options);
+    })
+  }
+  
+}
+
+
+function enableNotification(){
+  if('notification' in window && 'serviceWorker' in navigator){
+    window.Notification.requestPermission().then((choice)=>{
+      if(choice==='granted'){
+        $(".enable-notification").addClass('d-none');
+        configurePushSub();
+      }
+    });
+  }
+}
+
+
+function urlBase64ToUint8Array(base64String) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
